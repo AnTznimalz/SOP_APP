5c830f2c8bfc9c87b18e695e714f47163fe9f34b
toplordsaito <42961261+toplordsaito@users.noreply.github.com>
2020-08-25 14:53:52 +0700

    Change Example

--- week2/src/WeaponCollection/MainApp.java
+++ week2/src/WeaponCollection/MainApp.java
@@ -12,59 +12,27 @@ package WeaponCollection;
 public class MainApp {
 
     public static void main(String[] args) {
-//        word1
         WeaponMarget market = WeaponMarget.getInstance();
-//        ;
         System.out.println(market);
-//        Gun g = new Gun();
-//        market.OrderWeapon(g, 5);
-//        System.out.println(market.getCountWeapon());
+        Gun g = new Gun();
+        market.OrderWeapon(g, 2);
+        System.out.println(market.getCountWeapon());
         Sword s = new Sword();
         s.boost();
-//        s.boost();
         market.OrderWeapon(s, 5);
         System.out.println(market);
-//        System.out.println(market.getCountWeapon());
 
         Player player1 = new Player("player1");
         player1.Equip();
-
         player1.Equip();
-        player1.Equip();
-        player1.Equip();
-        player1.Equip();
-        player1.Equip();
-        player1.Equip();
-//        player1.Equip();
-//        System.out.println(market);
-        
-//        System.out.println(market);
-//        word2
-//        Player player2 = new Player("player2");
-//        player2.Equip();
+        System.out.println(market);
+        Player player2 = new Player("player2");
+        player2.Equip();
+        player2.Equip();
+        player2.Equip();
+        player1.unEquip();
         player1.showStat();
-//        player1.Equip();
-//        player2.unEquip();
-//        player1.Equip();
-//        player1.showStat();
-//        System.out.println(market);
-//        player2.unEquip();
-//        player1.Equip();
-//        player1.unEquip();
-//        System.out.println(market);
-//
-//        player1.Equip();
-//        player2.unEquip();
-//        player1.Equip();
-//        player1.showStat();
-//        player2.unEquip();
-//        player1.Equip();
-//        player2.showStat();
-//        player1.showStat();
-//        player1.Equip();
-//        player2.unEquip();
-//        player1.Equip();
-//        player1.showStat();
+        player2.showStat();
     }
 
 }
